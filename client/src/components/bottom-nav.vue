6ddea89643de495a7cd5c2c179652fbd69ca7c9f
Karol Horosin <karol.horosin@gmail.com>
2019-04-26 18:13:16 +0100

    Basic application layout

---
+++ client/src/components/bottom-nav.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-bottom-nav
+    :active.sync="bottomNav"
+    :value="true"
+    fixed
+    @update:active="changed"
+  >
+    <v-btn
+      color="teal"
+      flat
+      value="trasa"
+    >
+      <span>Remote access</span>
+      <v-icon>cloud</v-icon>
+    </v-btn>
+
+    <v-btn
+      color="teal"
+      flat
+      value="wars"
+    >
+      <span>Local access</span>
+      <v-icon>home</v-icon>
+    </v-btn>
+
+    <v-btn
+      color="teal"
+      flat
+      value="media"
+    >
+      <span>Settings</span>
+      <v-icon>settings</v-icon>
+    </v-btn>
+  </v-bottom-nav>
+</template>
+
+<script>
+  export default {
+    props: ['bottomNav'],
+    data () {
+      return {
+      }
+    },
+    methods: {
+      changed (val) {
+        this.$emit('bottom-menu-change', val)
+      }
+    }
+  }
+</script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-layout
+      wrap
+    >
+      <v-flex xs12>
+        <v-subheader>O aplikacji</v-subheader>
+        <v-card>
+          <v-card-title primary-title>
+            Test
+          </v-card-title>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    components: {
+    }
   }
-}
 </script>
