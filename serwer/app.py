f2f668a807676211a4cdb85369f929500bbf9728
Kamila <kamila.kalecinska@gmail.com>
2019-05-17 23:56:07 +0200

    basic version works

--- serwer/app.py
+++ serwer/app.py
@@ -3,6 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow 
 import os
 import datetime
+import requests
+from sqlalchemy.orm import sessionmaker
+
+ARDUINO_DATA_GET = 'http://192.168.5.18/arduino/digital/data'
+ARDUINO_STATUS_GET = 'http://192.168.5.18/arduino/digital/status'
+ARDUINO_OPEN_REQUEST = 'http://192.168.5.18/arduino/digital/open'
+ARDUINO_CLOSE_REQUEST = 'http://192.168.5.18/arduino/digital/close'
+timeout = 10
 
 # Init app
 app = Flask(__name__)
@@ -14,6 +22,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 # Init ma
 ma = Marshmallow(app)
+
+'''
+# create a configured "Session" class
+Session = sessionmaker(bind=db)
+# create a Session
+session = Session()
+'''
 
 # Product Class/Model
 class SensorsData(db.Model):
@@ -49,7 +64,7 @@ def hello():
     return jsonify({'msg': 'Hello Arduino'})
 
 # Add the data
-@app.route('/newdata', methods=['POST'])
+@app.route('/data/newdata', methods=['POST'])
 def add_data():
   temp = request.json['temp']
   hum = request.json['hum']
@@ -64,20 +79,21 @@ def add_data():
 
   return sensors_data_schema.jsonify(new_data)
 
-'''
+
 # Get History
-@app.route('/history', methods=['GET'])
-def get_products():
+@app.route('/data/all', methods=['GET'])
+def get_history():
   history = SensorsData.query.all()
   result = sensors_history_schema.dump(history)
   return jsonify(result.data)
 
 # Get Single Products
-@app.route('/product/<id>', methods=['GET'])
-def get_product(id):
-  product = Product.query.get(id)
-  return product_schema.jsonify(product)
+@app.route('/data/<id>', methods=['GET'])
+def get_data_by_id(id):
+  data = SensorsData.query.get(id)
+  return sensors_data_schema.jsonify(data)
 
+'''
 # Update a Product
 @app.route('/product/<id>', methods=['PUT'])
 def update_product(id):
@@ -96,15 +112,47 @@ def update_product(id):
   db.session.commit()
 
   return product_schema.jsonify(product)
-
-# Delete Product
-@app.route('/product/<id>', methods=['DELETE'])
-def delete_product(id):
-  product = Product.query.get(id)
-  db.session.delete(product)
-  db.session.commit()
-  return product_schema.jsonify(product)
 '''
+# Delete data
+@app.route('/data/<id>', methods=['DELETE'])
+def delete_data(id):
+  data = SensorsData.query.get(id)
+  db.session.delete(data)
+  db.session.commit()
+  return sensors_data_schema.jsonify(data)
+
+# Get status
+@app.route('/data/latest/status', methods=['GET'])
+def get_status():
+    last_record = SensorsData.query.order_by(SensorsData.id.desc()).first()
+    #status = {}
+    if last_record.open_status:
+        #status['open_status'] = "open"
+        status = "open"
+    else:
+        #status['open_status'] = "close"
+        status = "close"
+    return str(status)
+
+
+# Get latest data
+@app.route('/data/latest', methods=['GET'])
+def get_latest_data():
+    last_record = SensorsData.query.order_by(SensorsData.id.desc()).first()
+    return sensors_data_schema.jsonify(last_record)
+
+# Change status to open
+@app.route('/open', methods=['GET'])
+def open_request():
+    arduino_response = requests.get(ARDUINO_OPEN_REQUEST, timeout = timeout)
+    return str(arduino_response.text)
+
+
+# Change status to close
+@app.route('/close', methods=['GET'])
+def clsose_request():
+    arduino_response = requests.get(ARDUINO_CLOSE_REQUEST, timeout = timeout)
+    return str(arduino_response.text)
 
 # Run server
 if __name__ == '__main__':
--- serwer/ping_arduino.py
+++ serwer/ping_arduino.py
@@ -1,12 +1,28 @@
 import requests
 import json
+import time
 
 # Endpoints
 ARDUINO_DATA_GET = 'http://192.168.5.18/arduino/digital/data'
 ARDUINO_STATUS_GET = 'http://192.168.5.18/arduino/digital/status'
-API_NEWDATA_POST = 'http://localhost:5000/newdata'
+API_NEWDATA_POST = 'http://localhost:5000/data/newdata'
+ARDUINO_OPEN_REQUEST = 'http://192.168.5.18/arduino/digital/open'
+ARDUINO_CLOSE_REQUEST = 'http://192.168.5.18/arduino/digital/close'
+
 # Params
 headers = {'Content-Type': 'application/json',}
+timeout = 20
+MODE = 'automatic' # 'manual
+# Threasholds
+TEMP_LIMIT = 10.0 # close if < limit
+SOUND_LIMIT = 465 # close if > limit
+
+def close_request_by_limits(temp, sound):
+    if ((temp <= TEMP_LIMIT) or (sound >= SOUND_LIMIT)):
+        return "close"
+    else:
+        return None
+
 
 def parse_sensor_data(arduino_data, arduino_status):
     mydict = {}
@@ -19,12 +35,17 @@ def parse_sensor_data(arduino_data, arduino_status):
         mydict['open_status'] = True
     else:
         mydict['open_status'] = False
-    return json.dumps(mydict)
+    return mydict, json.dumps(mydict)
 
 
 if __name__ == '__main__':
-    data = requests.get(ARDUINO_DATA_GET)
-    status = requests.get(ARDUINO_STATUS_GET)
-    new_data = parse_sensor_data(data, status)
-    print(new_data)
-    new = requests.post(url = API_NEWDATA_POST, headers = headers, data = new_data)
+    while(True):
+        data = requests.get(ARDUINO_DATA_GET, timeout = timeout)
+        status = requests.get(ARDUINO_STATUS_GET, timeout = timeout)
+        new_data_dict, new_data = parse_sensor_data(data, status)
+        print(new_data)
+        new = requests.post(url = API_NEWDATA_POST, headers = headers, data = new_data)
+        if (MODE == "automatic"):
+            if (close_request_by_limits(new_data_dict['temp'], new_data_dict['sound']) is not None):
+                requests.get(ARDUINO_CLOSE_REQUEST, timeout = timeout)
+        time.sleep(50)
