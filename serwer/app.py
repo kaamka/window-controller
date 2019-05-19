e170e28173e96b061c748c77ec2498aba1a2887c
Kamila <kamila.kalecinska@gmail.com>
2019-05-19 02:31:34 +0200

    many devices work

--- serwer/app.py
+++ serwer/app.py
@@ -6,10 +6,11 @@ import datetime
 import requests
 from sqlalchemy.orm import sessionmaker
 
-ARDUINO_DATA_GET = 'http://192.168.5.18/arduino/digital/data'
-ARDUINO_STATUS_GET = 'http://192.168.5.18/arduino/digital/status'
-ARDUINO_OPEN_REQUEST = 'http://192.168.5.18/arduino/digital/open'
-ARDUINO_CLOSE_REQUEST = 'http://192.168.5.18/arduino/digital/close'
+ARDUINO = 'http://192.168.5.18'
+ARDUINO_DATA_GET = '/arduino/digital/data'
+ARDUINO_STATUS_GET = '/arduino/digital/status'
+ARDUINO_OPEN_REQUEST = '/arduino/digital/open'
+ARDUINO_CLOSE_REQUEST = '/arduino/digital/close'
 timeout = 10
 
 # Init app
@@ -30,32 +31,56 @@ Session = sessionmaker(bind=db)
 session = Session()
 '''
 
-# Product Class/Model
+# SensorsData Class/Model
 class SensorsData(db.Model):
   id = db.Column(db.Integer, primary_key=True)
+  device_id = db.Column(db.Integer, db.ForeignKey('device.id'), nullable=False)
+  device = db.relationship('Device',backref=db.backref('sensors_data', lazy=True))
   time = db.Column(db.DateTime, default=datetime.datetime.now())
   temp= db.Column(db.Float)
   hum = db.Column(db.Integer)
   sound = db.Column(db.Integer)
   gas = db.Column(db.Integer)
-  open_status = db.Column(db.Boolean)
+  open_status = db.Column(db.String(10))
 
-  def __init__(self, temp, hum, sound, gas, open_status):
+  def __init__(self, device_id, temp, hum, sound, gas, open_status):
+    self.device_id = device_id
     self.temp = temp
     self.hum = hum
     self.sound = sound
     self.gas = gas
     self.open_status = open_status
 
+class Device(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    ip_address = db.Column(db.String(50), nullable=False)
+    def __init__(self, name, ip_address):
+        self.name = name
+        self.ip_address = ip_address
+
+    def __repr__(self):
+        return '<Device %r : %r>' % self.name, self.ip_address
+
 
 # Sensor data Schema
 class SensorsDataSchema(ma.Schema):
   class Meta:
-    fields = ('id', 'time', 'temp', 'hum', 'sound', 'gas', 'open_status')
+    fields = ('id', 'device_id', 'time', 'temp', 'hum', 'sound', 'gas', 'open_status')
 
 # Init schema
 sensors_data_schema = SensorsDataSchema(strict=True)
 sensors_history_schema = SensorsDataSchema(many=True, strict=True)
+
+
+# Devices Schema
+class DeviceSchema(ma.Schema):
+  class Meta:
+    fields = ('id', 'name', 'ip_address')
+
+# Init devices schema
+device_schema = DeviceSchema(strict=True)
+all_devices_schema = DeviceSchema(many=True, strict=True)
 
 
 # Home
@@ -63,16 +88,30 @@ sensors_history_schema = SensorsDataSchema(many=True, strict=True)
 def hello():
     return jsonify({'msg': 'Hello Arduino'})
 
+# Add the device
+@app.route('/devices/new', methods=['POST'])
+def add_device():
+  name = request.json['name']
+  ip_address = request.json['ip_address']
+  new_device = Device(name, ip_address)
+
+  db.session.add(new_device)
+  db.session.commit()
+
+  return device_schema.jsonify(new_device)
+
+
 # Add the data
 @app.route('/data/newdata', methods=['POST'])
 def add_data():
+  device_id = request.json['device_id']
   temp = request.json['temp']
   hum = request.json['hum']
   sound = request.json['sound']
   gas = request.json['gas']
   open_status = request.json['open_status']
 
-  new_data = SensorsData(temp, hum, sound, gas, open_status)
+  new_data = SensorsData(device_id, temp, hum, sound, gas, open_status)
 
   db.session.add(new_data)
   db.session.commit()
@@ -87,32 +126,45 @@ def get_history():
   result = sensors_history_schema.dump(history)
   return jsonify(result.data)
 
-# Get Single Products
+# Get history of one device
+#sensors_data
+@app.route('/data/all/<device_id>', methods=['GET'])
+def get_data_of_device(device_id):
+  r = SensorsData.query.filter_by(device_id = device_id)
+  result = sensors_history_schema.dump(r)
+  return jsonify(result.data)
+
+# Get Single data record
 @app.route('/data/<id>', methods=['GET'])
 def get_data_by_id(id):
   data = SensorsData.query.get(id)
   return sensors_data_schema.jsonify(data)
 
-'''
-# Update a Product
-@app.route('/product/<id>', methods=['PUT'])
-def update_product(id):
-  product = Product.query.get(id)
+# Get All Devices
+@app.route('/devices/all', methods=['GET'])
+def get_all_devices():
+  devices = Device.query.all()
+  result = all_devices_schema.dump(devices)
+  return jsonify(result.data)
 
+# Get Single Device
+@app.route('/devices/<id>', methods=['GET'])
+def get_device_by_id(id):
+  data = Device.query.get(id)
+  return device_schema.jsonify(data)
+
+
+# Update a Device
+@app.route('/devices/<id>', methods=['PUT'])
+def update_device(id):
+  dev = Device.query.get(id)
   name = request.json['name']
-  description = request.json['description']
-  price = request.json['price']
-  qty = request.json['qty']
-
-  product.name = name
-  product.description = description
-  product.price = price
-  product.qty = qty
-
+  ip_address = request.json['ip_address']
+  dev.name = name
+  dev.ip_address = ip_address
   db.session.commit()
+  return device_schema.jsonify(dev)
 
-  return product_schema.jsonify(product)
-'''
 # Delete data
 @app.route('/data/<id>', methods=['DELETE'])
 def delete_data(id):
@@ -121,37 +173,44 @@ def delete_data(id):
   db.session.commit()
   return sensors_data_schema.jsonify(data)
 
+# Delete device 
+@app.route('/devices/<id>', methods=['DELETE'])
+def delete_device_by_id(id):
+  data = Device.query.get(id)
+  db.session.delete(data)
+  db.session.commit()
+  return device_schema.jsonify(data)
+
+## Add device id
 # Get status
-@app.route('/data/latest/status', methods=['GET'])
-def get_status():
-    last_record = SensorsData.query.order_by(SensorsData.id.desc()).first()
-    #status = {}
-    if last_record.open_status:
-        #status['open_status'] = "open"
-        status = "open"
-    else:
-        #status['open_status'] = "close"
-        status = "close"
-    return str(status)
+@app.route('/data/latest/status/<device_id>', methods=['GET'])
+def get_status(device_id):
+    last_record = SensorsData.query.filter_by(device_id = device_id).order_by(SensorsData.id.desc()).first()
+    return str(last_record.open_status)
 
-
+## Add device id
 # Get latest data
-@app.route('/data/latest', methods=['GET'])
-def get_latest_data():
-    last_record = SensorsData.query.order_by(SensorsData.id.desc()).first()
+@app.route('/data/latest/<device_id>', methods=['GET'])
+def get_latest_data(device_id):
+    last_record = SensorsData.query.filter_by(device_id = device_id).order_by(SensorsData.id.desc()).first()
     return sensors_data_schema.jsonify(last_record)
 
+
 # Change status to open
-@app.route('/open', methods=['GET'])
-def open_request():
-    arduino_response = requests.get(ARDUINO_OPEN_REQUEST, timeout = timeout)
+@app.route('/open/<device_id>', methods=['GET'])
+def open_request(device_id):
+    dev = Device.query.get(device_id)
+    url = dev.ip_address + ARDUINO_OPEN_REQUEST
+    arduino_response = requests.get(url, timeout = timeout)
     return str(arduino_response.text)
 
 
 # Change status to close
-@app.route('/close', methods=['GET'])
-def close_request():
-    arduino_response = requests.get(ARDUINO_CLOSE_REQUEST, timeout = timeout)
+@app.route('/close/<device_id>', methods=['GET'])
+def close_request(device_id):
+    dev = Device.query.get(device_id)
+    url = dev.ip_address + ARDUINO_CLOSE_REQUEST
+    arduino_response = requests.get(url, timeout = timeout)
     return str(arduino_response.text)
 
 '''
--- serwer/ping_arduino.py
+++ serwer/ping_arduino.py
@@ -3,11 +3,14 @@ import json
 import time
 
 # Endpoints
-ARDUINO_DATA_GET = 'http://192.168.5.18/arduino/digital/data'
-ARDUINO_STATUS_GET = 'http://192.168.5.18/arduino/digital/status'
+#http://192.168.5.18
+ARDUINO_DATA_GET = '/arduino/digital/data'
+ARDUINO_STATUS_GET = '/arduino/digital/status'
+ARDUINO_OPEN_REQUEST = '/arduino/digital/open'
+ARDUINO_CLOSE_REQUEST = '/arduino/digital/close'
+
 API_NEWDATA_POST = 'http://localhost:5000/data/newdata'
-ARDUINO_OPEN_REQUEST = 'http://192.168.5.18/arduino/digital/open'
-ARDUINO_CLOSE_REQUEST = 'http://192.168.5.18/arduino/digital/close'
+API_ALL_DEVICES = 'http://localhost:5000/devices/all'
 
 # Params
 headers = {'Content-Type': 'application/json',}
@@ -31,23 +34,27 @@ def parse_sensor_data(arduino_data, arduino_status):
     mydict['hum'] = int(mylist[2])
     mydict['sound'] = int(mylist[1])
     mydict['gas'] = int(mylist[0])
-    if arduino_status.text == 'open':
-        mydict['open_status'] = True
-    else:
-        mydict['open_status'] = False
-    return mydict, json.dumps(mydict)
+    mydict['open_status'] = arduino_status.text
+    return mydict
 
 
 if __name__ == '__main__':
+    devices_json = requests.get(API_ALL_DEVICES).text
+    devices = json.loads(devices_json) # list of dics
     while(True):
-        data = requests.get(ARDUINO_DATA_GET, timeout = timeout)
-        status = requests.get(ARDUINO_STATUS_GET, timeout = timeout)
-        new_data_dict, new_data = parse_sensor_data(data, status)
-        print(new_data)
-        new = requests.post(url = API_NEWDATA_POST, headers = headers, data = new_data)
-        '''
-        if (MODE == "automatic"):
-            if (close_request_by_limits(new_data_dict['temp'], new_data_dict['sound']) is not None):
-                requests.get(ARDUINO_CLOSE_REQUEST, timeout = timeout)
+        for device in devices:
+            IP = device['ip_address']
+            device_id = device['id']
+            data = requests.get(IP+ARDUINO_DATA_GET, timeout = timeout)
+            status = requests.get(IP+ARDUINO_STATUS_GET, timeout = timeout)
+            new_data_dict = parse_sensor_data(data, status)
+            new_data_dict['device_id'] = device_id
+            new_data = json.dumps(new_data_dict)
+            print(new_data)
+            new = requests.post(url = API_NEWDATA_POST, headers = headers, data = new_data)
+            '''
+            if (MODE == "automatic"):
+                if (close_request_by_limits(new_data_dict['temp'], new_data_dict['sound']) is not None):
+                    requests.get(ARDUINO_CLOSE_REQUEST, timeout = timeout)
         '''
         time.sleep(50)
